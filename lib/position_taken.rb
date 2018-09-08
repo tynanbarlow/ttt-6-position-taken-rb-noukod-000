7e358c445cc21519cc370c6958bce2388ee1823d
Tynan Barlow <tynan@noukod.org>
2018-09-08 18:46:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
 
-  if board[index] == " " || nil  then
+  if board[index] == " " || "" || nil  then
     return false
   elsif board[index] == "X" || "O" then
     return true
